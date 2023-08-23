4eefd91b96fed1b610ce09bf82fab9043958a377
Vihenrie <viniciushpintolp@gmail.com>
2023-08-23 08:55:45 -0300

    Add: Buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,13 +1,20 @@
 import styles from './Cardapio.module.scss'
-import logo from '../../assets/logo.svg'
+import logo from 'assets/logo.svg'
 
 export default function Cardapio() {
     return(
         <main>
             <nav className={ styles.menu }>
-                <img src={logo} alt="logo casa del taco
-                " />
+                <img src={logo} alt="logo casa del taco" />
             </nav>
+            <header className={ styles.header }>
+                <div className={ styles.header__text }>
+                    A casa del taco y del codigo
+                </div>
+            </header>
+            <section>
+                <h3>Cardápio</h3>
+            </section>
         </main>
     );
 }
